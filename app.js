642662109f3a0bdfba0ff9bfda00ee36838bafd4
smccourtb <shawnmccourtb@gmail.com>
2021-09-15 15:51:03 -0400

    add GameState object and refactored click event from gameboard to gamestate. Alternates players with each click and updates the board appropriately.

--- app.js
+++ app.js
@@ -3,21 +3,16 @@ const Gameboard = (() => {
     let gameboard = ["", "", "",
                      "", "", "",
                      "", "", ""]
-    
-    
     const clear = () => {
         for(let i = 0; i < gameboard.length; i++){
             update(i, "");
         };
     };
-
     const update = (index, letter) => {
         gameboard[index] = letter;
         const x = document.getElementById(index)
         x.textContent = letter;
-
     };
-
     const initialize = (function(size=9) {
         const clearButton = document.querySelector(".clear")
         clearButton.addEventListener("click", clear)
@@ -26,22 +21,49 @@ const Gameboard = (() => {
             const cell = document.createElement('div')
             cell.classList.add("cell")
             cell.setAttribute("id", i)
-            cell.addEventListener('click', () => {
-                update(cell.getAttribute('id'), "X")})
             container.append(cell)
-
         };
     })();
-    return {clear, update, gameboard}
+    return {update}
 })()
 
-const Player = ((name, number) => {
-    let name = name
-    let number = number
+const Player = ((representation) => {
+    let letter = representation
+    let gamesWon = 0
 
-    
-})
+    return {gamesWon, letter} ;  
+});
 
 const GameState = (() => {
-    pass
-})
+    //establish player 1 and player 2
+    const player1 = Player("X");
+    const player2 = Player("O");
+    //establish gameboard
+    const board = Gameboard;
+    let turn = 0;
+    let current_player = player1;
+    const addEvent = (() => {
+        for(let i = 0; i < 9; i++){
+            const cell = document.getElementById(i)
+            cell.addEventListener('click', () => {
+                board.update(cell.getAttribute('id'), current_player.letter);
+                turn += 1;
+                if(current_player===player1) {
+                    console.log(current_player)
+                    current_player = player2;
+                } 
+                else {
+                    console.log(current_player)
+                    current_player = player1;
+                }
+                console.log(turn)
+            });
+    };
+})();
+    //listen for player 1 click
+    //update gameboard on click
+    //check for win state
+    //listen for player 2 click
+    return {player1, player2, board, turn, current_player}
+
+})();
