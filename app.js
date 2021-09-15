85e6dc8d0fcdb8c16c2b4fb0de3187b7616fa005
smccourtb <shawnmccourtb@gmail.com>
2021-09-15 14:08:05 -0400

    add event listeners to clear button and gameboard.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Gameboard = (() => {
                      "", "", "",
                      "", "", ""]
     
+    
     const clear = () => {
         for(let i = 0; i < gameboard.length; i++){
             update(i, "");
@@ -12,18 +13,35 @@ const Gameboard = (() => {
 
     const update = (index, letter) => {
         gameboard[index] = letter;
+        const x = document.getElementById(index)
+        x.textContent = letter;
+
     };
 
     const initialize = (function(size=9) {
+        const clearButton = document.querySelector(".clear")
+        clearButton.addEventListener("click", clear)
         const container = document.querySelector(".grid")
         for(let i = 0; i < size; i++){
             const cell = document.createElement('div')
             cell.classList.add("cell")
-            cell.addEventListener('click', () =>
-            cell.textContent = "X")
+            cell.setAttribute("id", i)
+            cell.addEventListener('click', () => {
+                update(cell.getAttribute('id'), "X")})
             container.append(cell)
 
         };
     })();
-    return {clear, update, initialize}
+    return {clear, update, gameboard}
 })()
+
+const Player = ((name, number) => {
+    let name = name
+    let number = number
+
+    
+})
+
+const GameState = (() => {
+    pass
+})
