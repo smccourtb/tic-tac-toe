7f4e8048938e21bc8d8f1711bdfeabef1b519441
smccourtb <shawnmccourtb@gmail.com>
2021-09-16 13:20:03 -0400

    display player names and tracks/displays individual scores.

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const Gameboard = (() => {
     const state = (player) => {
         let index = []
         for(let i = 0; i < gameboard.length; i++) {
-            if(gameboard[i] === player) {
+            if(gameboard[i] === player.letter) {
                 index.push(i);
             };
         };
@@ -47,15 +47,17 @@ const Gameboard = (() => {
                 for(j of combo) {
                     if(temp.includes(j)) {
                         count += 1;
-                        if (count> 2) {
-                            alert(`${player} wins`);
+                        if (count > 2) {
+                            alert(`${player.name} Wins!`);
                             clear();
+                            return true
                         };
                     };
                 };
             };
             if (tie()) {
                 alert("It's a Tie!")
+                return true
             };
         };
     };
@@ -66,42 +68,68 @@ const Gameboard = (() => {
         }
     };
 
-    return {update, state, tie};
+    return {update, state};
 })();
 
-const Player = ((representation) => {
+const Player = ((representation, id) => {
+    let name = ""
     let letter = representation;
     let gamesWon = 0;
-    return {gamesWon, letter};  
+    let playerID  = id
+    const set_name = (() => {
+        let nameChoice = prompt('Enter Your Name: ');
+        name = nameChoice
+    })();
+    return {gamesWon, letter, name, playerID};  
 });
 
-const GameState = (() => {
+const Game = (() => {
     //establish player 1 and player 2
-    const player1 = Player("X");
-    const player2 = Player("O");
+    const player1 = Player("X", 1);
+    const player2 = Player("O", 2);
     //establish gameboard
     const board = Gameboard;
     // set first player
-    let current_player = player1;
+    let currentPlayer = player1;
+    // get DOM elemnts
+    const player1Panel = document.getElementById("player1");
+    const player2Panel = document.getElementById("player2");
+    
+    const setupPanels = (() => {
+        const player1Title = document.createElement('h1');
+        const player2Title = document.createElement('h1');
+        const player1Score = document.createElement('h1');
+        const player2Score = document.createElement('h1');
+        player1Score.setAttribute('id', "player1Score")
+        player2Score.setAttribute('id', "player2Score")
+        player1Title.textContent = player1.name
+        player2Title.textContent = player2.name
+        player1Panel.append(player1Title, player1Score);
+        player2Panel.append(player2Title, player2Score);
+    })();
     // add click event listener to gameboard
     const addEvent = (() => {
         for(let i = 0; i < 9; i++){
             const cell = document.getElementById(i);
             cell.addEventListener('click', () => {
                 if (cell.textContent === "") {
-                    board.update(cell.getAttribute('id'), current_player.letter);
-                    board.state(current_player.letter);
-                    
-                    if(current_player===player1) {
-                        current_player = player2;
-                    } 
-                    else {
-                        current_player = player1;
+                    board.update(cell.getAttribute('id'), currentPlayer.letter);
+                    var winCheck = (board.state(currentPlayer));
+                    if (winCheck){
+                        currentPlayer.gamesWon += 1
+                        const scorePanel = document.getElementById(`player${currentPlayer.playerID}Score`)
+                        scorePanel.textContent = currentPlayer.gamesWon
                     }
+                    if(currentPlayer===player1) {
+                        currentPlayer = player2;
+                    }
+                    else {
+                        currentPlayer = player1;
+                    };
                 
                 };
             });
         };
     })();
-    return {player1, player2, board, current_player};
+    return {player1, player2, board, currentPlayer};
 })();
