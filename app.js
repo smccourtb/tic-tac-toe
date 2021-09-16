cde6924b505a695d7265651d3e9a7e3c5144673b
smccourtb <shawnmccourtb@gmail.com>
2021-09-16 10:59:33 -0400

    fix bug for validating an invalid win as true. Create alert box indicating which player won, and clear the board afterwards automatically.

--- app.js
+++ app.js
@@ -38,13 +38,16 @@ const Gameboard = (() => {
         for(combo of winningCombinations){
             // make a copy of index
             let temp = index
+            let count = 0
             if (temp.length > 2){
             // go thorugh each element of temp and check if that elemnt appears in combo
                 for(j of combo){
                     if(temp.includes(j)) {
-                        // remove the element from temp
-                     
-                        return console.log(`${player} wins`)
+                        count += 1
+                        if (count> 2){
+                            alert(`${player} wins`)
+                            clear()
+                        }
                     }
                 }
          }
@@ -66,7 +69,6 @@ const GameState = (() => {
     const player2 = Player("O");
     //establish gameboard
     const board = Gameboard;
-    let turn = 0;
     let current_player = player1;
     const addEvent = (() => {
         for(let i = 0; i < 9; i++){
@@ -74,7 +76,6 @@ const GameState = (() => {
             cell.addEventListener('click', () => {
                 board.update(cell.getAttribute('id'), current_player.letter);
                 board.state(current_player.letter)
-                turn += 1;
                 if(current_player===player1) {
                     console.log(current_player)
                     current_player = player2;
@@ -84,14 +85,10 @@ const GameState = (() => {
                     current_player = player1;
                 }
                 
-                console.log(turn)
             });
     };
 })();
-    //listen for player 1 click
-    //update gameboard on click
-    //check for win state
-    //listen for player 2 click
-    return {player1, player2, board, turn, current_player}
+
+    return {player1, player2, board, current_player}
 
 })();
